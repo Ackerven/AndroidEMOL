7f980d67bc28503f94451bb52d8ef8c43f9d0ebf
Ackerven <ackerven@gmail.com>
2022-12-11 14:23:37 +0800

    fix: foreign key

--- app/src/main/java/com/ackerven/adnroid/emol/model/Diary.kt
+++ app/src/main/java/com/ackerven/adnroid/emol/model/Diary.kt
@@ -1,16 +1,21 @@
 package com.ackerven.adnroid.emol.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "diary", foreignKeys = [ForeignKey(
-    entity = Moon::class, parentColumns = arrayOf("mood"), childColumns = arrayOf("id"))]
+@Entity(
+    tableName = "diary", foreignKeys = [ForeignKey(
+        entity = Moon::class, parentColumns = arrayOf("id"), childColumns = arrayOf("mood")
+    )]
 )
-data class Diary(@PrimaryKey val id: UUID = UUID.randomUUID(),
-                 var date: Date,
-                 var mood: UUID,
-                 var title: String,
-                 var text: String,
-                 var isDelete: Boolean = false)
+data class Diary(
+    @PrimaryKey val id: UUID = UUID.randomUUID(),
+    var date: Date,
+    @ColumnInfo(index = true) var mood: UUID,
+    var title: String,
+    var text: String,
+    var isDelete: Boolean = false
+)
